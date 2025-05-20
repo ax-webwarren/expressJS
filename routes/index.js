6fb1988accdca1d18acb8df9898ef6356a155cd6
ax-webwarren <warrentulangdano@gmail.com>
2025-05-20 17:05:48 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,77 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+var db = require('../lib/connection');
+
+/* GET home page. 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+*/
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  db.any('SELECT * from public.pages')
+  .then((data) => {
+    console.log(data);
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(
+      {
+        'status': 500,
+        'message': error
+      }
+    )
+  });
+});
+
+router.delete('/', function(req, res, next) {
+  db.any('delete from public.pages where title = $1', req.body.title)
+  .then((data) => {
+    console.log(data);
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(
+      {
+        'status': 500,
+        'message': error
+      }
+    )
+  });
+});
+
+router.put('/', function(req, res, next) {
+  console.log(req);
+  db.any('update public.pages SET title = $1 where title = $2', [req.body.title, req.body.old_title])
+  .then((data) => {
+    console.log(data);
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(
+      {
+        'status': 500,
+        'message': error
+      }
+    )
+  });
+});
+
+router.post('/', function(req, res, next) {
+  db.any('insert into public.pages (title, content, slug) values ($1, $2, $3)', [req.body.title, req.body.content, req.body.slug])
+  .then((data) => {
+    console.log(data);
+    res.send(data);
+  })
+  .catch((error) => {
+    res.send(
+      {
+        'status': 500,
+        'message': error
+      }
+    )
+  });
 });
 
 module.exports = router;
